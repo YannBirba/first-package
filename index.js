f71000c7e3a70ad95170a8edd3d235f795e6a3fc
Yann Birba <birbayann@gmail.com>
2022-10-12 14:32:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const add = (/** @type {number} */ num1, /** @type {number} */ num2) => {
+export const add = (/** @type {number} */ num1, /** @type {number} */ num2) => {
   return num1 + num2;
 };
 
-const subtract = (/** @type {number} */ num1, /** @type {number} */ num2) => {
+export const subtract = (
+  /** @type {number} */ num1,
+  /** @type {number} */ num2
+) => {
   return num1 - num2;
 };
-
-exports.add = add;
-exports.subtract = subtract;
